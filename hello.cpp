a9b0e058ffb6e853ce5198504b84322a8e7f4b88
WJH <3506456886@qq.com>
2025-11-09 17:18:04 +0800

    feat: 注释

--- hello.cpp
+++ hello.cpp
@@ -2,13 +2,13 @@
 #include<string>
 using namespace std;
 void bonjour(){
-    cout<<"输入您的名字";
-    string name;
-    cin>>name;
-    cout<<"bonjour "<<name<<'\n';
+    cout<<"输入您的名字"; // 输入
+    string name; // 定义变量类型
+    cin>>name; // 输入
+    cout<<"bonjour "<<name<<'\n'; // 格式化输出
 }
 
-int main(){
+int main(){ // 主函数调用上面定义的功能函数实现模块化区分、代码的松耦合
     bonjour();
     return 0;
 }
