a6e72365de5ed883a7bbaeba966404e6a947242e
WJH <3506456886@qq.com>
2026-01-19 21:17:27 +0800

    feat: Q8

--- .2025/Ex75/Q8.h
+++ .2025/Ex75/Q8.h
@@ -6,6 +6,6 @@ std::ostream &operator<<(std::ostream &f, const relation<A, B> &r)
 {
     f << "Relation " << r.get_nom() << std::endl;
     for (relation<A, B>::iterator it = r.begin(); it != r.end(); ++it)
-        f << (*it).first << r, get_symbole() << (*it).second; // 最好还是别用箭头因为不一定实现了 it->second;
+        f << (*it).first << r.get_symbole() << (*it).second; // 最好还是别用箭头因为不一定实现了 it->second;
     return f;
 }
