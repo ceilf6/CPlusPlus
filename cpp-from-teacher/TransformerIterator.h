c61860e517e92b1189805df3eb350a0c3d75884d
WJH <3506456886@qq.com>
2026-01-19 16:54:14 +0800

    feat: 2025 final

--- cpp-from-teacher/TransformerIterator.h
+++ cpp-from-teacher/TransformerIterator.h
@@ -4,28 +4,32 @@
 #include <string>
 
 template <class CONTAINER, class Transformer>
-class TransformerIterator {
+class TransformerIterator
+{
 private:
-    const CONTAINER& container;
+    // CONTAINER 泛型到时候应该就是 vector
+    const CONTAINER &container;
     typename CONTAINER::const_iterator current;
     Transformer transformer;
 
 public:
-    TransformerIterator(const CONTAINER& c, Transformer t)
+    TransformerIterator(const CONTAINER &c, Transformer t)
         : container(c), current(container.begin()), transformer(t) {}
 
-    void operator++() {
+    void operator++()
+    {
         ++current;
     }
 
-    bool goodbon() const {
+    bool goodbon() const
+    {
         return current != container.end();
     }
 
-    std::string operator*() const {
+    std::string operator*() const
+    {
         return transformer(*current);
     }
 };
 
 #endif
-
